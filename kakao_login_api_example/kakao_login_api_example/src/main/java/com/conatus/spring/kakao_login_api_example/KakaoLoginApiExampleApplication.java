0503ab6fd58eed6cd50905ca64ea800384c61d9a
conatus-recedendi <kylee@student.42seoul.kr>
2021-07-22 08:40:34 +0900

    feat: add the project for kakao api login

---
+++ kakao_login_api_example/kakao_login_api_example/src/main/java/com/conatus/spring/kakao_login_api_example/KakaoLoginApiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.conatus.spring.kakao_login_api_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoLoginApiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoLoginApiExampleApplication.class, args);
+	}
+
+}
---
+++ kakao_login_api_example/kakao_login_api_example/src/test/java/com/conatus/spring/kakao_login_api_example/KakaoLoginApiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conatus.spring.kakao_login_api_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoLoginApiExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
